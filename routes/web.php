09c4ffa1550588b94c4eb36c02a9fa5982a3958c
BiagioCaputo <scebiagio98@gmail.com>
2024-03-20 20:10:14 +0100

    Created a show blade for the user with the slug as reference for the project

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
-use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use App\Http\Controllers\Guest\ProjectController as GuestProjectController;
+
+
 
 
 use App\Http\Controllers\ProfileController;
@@ -22,22 +25,25 @@ use Illuminate\Support\Facades\Route;
 //Rotta home guest
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
+Route::get('/projects/{slug}', [App\Http\Controllers\Guest\ProjectController::class, 'show'])->name('guest.projects.show'); // non funziona l'alias...
+
+
 
 
 Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function(){
     //Rotta admin home
     Route::get('', AdminHomeController::class)->name('home');
-    //Rotte admin post
+    /*Rotte admin post
     Route::get('/projects', [ProjectController::class,'index'])->name('projects.index');
     Route::get('/projects/create', [ProjectController::class,'create'])->name('projects.create');
     Route::get('/projects/{project}', [ProjectController::class,'show'])->name('projects.show');
     Route::post('/projects', [ProjectController::class,'store'])->name('projects.store');
     Route::get('/projects/{project}/edit', [ProjectController::class,'edit'])->name('projects.edit');
     Route::put('/projects/{project}', [ProjectController::class,'update'])->name('projects.update');
-    Route::delete('/projects/{project}', [ProjectController::class,'destroy'])->name('projects.destroy');
+    Route::delete('/projects/{project}', [ProjectController::class,'destroy'])->name('projects.destroy');*/
 
-    
-    //Route::resource('projects', Projecontroller::class);
+
+    Route::resource('projects', AdminProjectcontroller::class);
 });
  
 
